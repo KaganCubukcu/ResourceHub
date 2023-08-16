795c1d896dd6f6d8b6cf0fadc6810fc22623b8c7
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-08-16 14:30:18 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <the-header title="Resource Hub"></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheResources from "./components/learning-resources/TheResources.vue";
+export default {
+  components: {
+    TheHeader,
+    TheResources,
+  },
+};
+</script>
